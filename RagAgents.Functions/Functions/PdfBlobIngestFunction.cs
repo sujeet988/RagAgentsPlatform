0e7988884159b8155da92e095c4736376c134b92
sujeet kumar <sujeetkumar2010@live.com>
2025-12-21 12:09:21 +0530

    Blob Functions

--- RagAgents.Functions/Functions/PdfBlobIngestFunction.cs
+++ RagAgents.Functions/Functions/PdfBlobIngestFunction.cs
@@ -1,24 +1,34 @@
-using System.IO;
-using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using RagAgents.Core.Interfaces;
+using RagAgents.Core.Services;
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
 
 namespace RagAgents.Functions.Functions;
 
 public class PdfBlobIngestFunction
 {
+    private readonly IPdfIngestService _pdfIngestService;
     private readonly ILogger<PdfBlobIngestFunction> _logger;
 
-    public PdfBlobIngestFunction(ILogger<PdfBlobIngestFunction> logger)
+    public PdfBlobIngestFunction(IPdfIngestService pdfIngestService, ILogger<PdfBlobIngestFunction> logger)
     {
+        _pdfIngestService = pdfIngestService;
         _logger = logger;
     }
 
     [Function(nameof(PdfBlobIngestFunction))]
-    public async Task Run([BlobTrigger("samples-workitems/{name}", Connection = "")] Stream stream, string name)
+    public async Task Run([BlobTrigger("samples-workitems/{name}", Connection = "")] Stream blobStream, string name)
     {
-        using var blobStreamReader = new StreamReader(stream);
+        using var blobStreamReader = new StreamReader(blobStream);
         var content = await blobStreamReader.ReadToEndAsync();
         _logger.LogInformation("C# Blob trigger function Processed blob\n Name: {name} \n Data: {content}", name, content);
+
+        await _pdfIngestService.IngestAsync(blobStream, name);
+
+        _logger.LogInformation($"Blob {name} ingested successfully");
+       
     }
 }
