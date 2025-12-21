2ff253a5118b13e749c550f614d9c0b325fd84fa
sujeet kumar <sujeetkumar2010@live.com>
2025-12-21 11:35:18 +0530

    Conversation Store

--- RagAgents.Core/Services/CosmosConversationStore.cs
+++ RagAgents.Core/Services/CosmosConversationStore.cs
@@ -1,28 +1,61 @@
 ﻿
+using Microsoft.Azure.Cosmos;
+using Microsoft.VisualBasic;
 using RagAgents.Core.Interfaces;
 using RagAgents.Core.Models;
 
 
 namespace RagAgents.Core.Services
 {
-    public class CosmosConversationStore // : IConversationStore
+    public class CosmosConversationStore  : IConversationStore
     {
-        //private readonly Container _container;
-        //public CosmosConversationStore(cosmo client)
-        //{
-        //    _container = client
-        // .GetDatabase("rag-chat-db")
-        // .GetContainer("conversations");
+        private readonly Container _container;
+        public CosmosConversationStore(CosmosClient client)
+        {
+          _container = client
+         .GetDatabase("rag-chat-db")
+         .GetContainer("conversations");
 
-        //}
-        //public Task<IReadOnlyList<ChatMessageModel>> GetHistoryAsync(string conversationId, int maxMessages)
-        //{
-        //    throw new NotImplementedException();
-        //}
+        }
+        public async Task<IReadOnlyList<ChatMessageModel>> GetHistoryAsync(string conversationId, int maxMessages)
+        {
+            try
+            {
+                var query = new QueryDefinition("SELECT * FROM c WHERE c.conversationId = @cid ORDER BY c.timestamp DESC")
+                .WithParameter("@cid", conversationId);
 
-        //public Task SaveMessageAsync(ChatMessageModel message)
-        //{
-        //    throw new NotImplementedException();
-        //}
+                var iterator = _container.GetItemQueryIterator<ChatMessageModel>(
+                    query,
+                    requestOptions: new QueryRequestOptions
+                    {
+                        PartitionKey = new PartitionKey(conversationId),
+                        MaxItemCount = maxMessages
+                    });
+
+                var results = new List<ChatMessageModel>();
+
+                while (iterator.HasMoreResults && results.Count < maxMessages)
+                {
+                    var response = await iterator.ReadNextAsync();
+                    results.AddRange(response);
+                }
+
+                // Return oldest → newest
+                return results
+                    .OrderBy(m => m.Timestamp)
+                    .ToList();
+            }
+            catch(Exception ex)
+            {
+                 throw;
+            }
+        }
+
+        public async Task SaveMessageAsync(ChatMessageModel message)
+        {
+            await _container.CreateItemAsync(
+             message,
+             new PartitionKey(message.ConversationId));
+        }
     }
 }
