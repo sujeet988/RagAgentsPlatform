5cdd4f2c65259ff35ddbbb94aa05e035e24b24fc
sujeet kumar <sujeetkumar2010@live.com>
2025-12-21 12:58:03 +0530

    Written Config value code

--- RagAgents.Functions/Program.cs
+++ RagAgents.Functions/Program.cs
@@ -1,7 +1,12 @@
+using Azure;
+using Azure.AI.FormRecognizer.DocumentAnalysis;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RagAgents.Core.Interfaces;
+using RagAgents.Core.Services;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
@@ -10,5 +15,23 @@ builder.ConfigureFunctionsWebApplication();
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights();
+
+// get all config values
+var config = builder.Configuration;
+var result = config["DocumentAI:Endpoint"];
+
+//ADD YOUR SERVICES HERE 
+builder.Services.AddSingleton<IPdfIngestService, PdfIngestService>();
+builder.Services.AddSingleton<IRagService, RagService>();
+
+// Register DocumentAnalysisClient for Form Recognizer
+builder.Services.AddSingleton(sp =>
+{
+    var config = sp.GetRequiredService<IConfiguration>();
+
+    return new DocumentAnalysisClient(
+        new Uri(config["DocumentAI:Endpoint"]),
+        new AzureKeyCredential(config["DocumentAI:Key"]));
+});
 
 builder.Build().Run();
