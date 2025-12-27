3810db46d41f27c96797f1e9fbc3b18da61a9500
sujeet kumar <sujeetkumar2010@live.com>
2025-12-27 15:08:18 +0530

    Conversation History

---
+++ RagAgents.Core/Interfaces/IConversationStoreInMemory.cs
@@ -0,0 +1,16 @@
+﻿using RagAgents.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RagAgents.Core.Interfaces
+{
+    public interface IConversationStoreInMemory
+    {
+        Task<IReadOnlyList<ChatMessageModel>> GetHistoryAsync(string conversationId, string userId,int maxMessages);
+
+        Task SaveMessageAsync(ChatMessageModel message);
+    }
+}
---
+++ RagAgents.Core/Services/InMemoryConversationStore.cs
@@ -0,0 +1,34 @@
+﻿using RagAgents.Core.Interfaces;
+using RagAgents.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RagAgents.Core.Services
+{
+    public class InMemoryConversationStore : IConversationStoreInMemory
+    {
+        private static readonly List<ChatMessageModel> _messages = new();
+        public Task<IReadOnlyList<ChatMessageModel>> GetHistoryAsync(string conversationId, string userId, int maxMessages)
+        {
+            var history = _messages
+           .Where(m =>
+               m.ConversationId == conversationId &&
+               m.UserId == userId)
+           .OrderByDescending(m => m.Timestamp)
+           .Take(maxMessages)
+           .OrderBy(m => m.Timestamp)
+           .ToList();
+
+            return Task.FromResult<IReadOnlyList<ChatMessageModel>>(history);
+        }
+
+        public Task SaveMessageAsync(ChatMessageModel message)
+        {
+            _messages.Add(message);
+            return Task.CompletedTask;
+        }
+    }
+}
